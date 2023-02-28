b4b761f8bc009340b68b5ee9b752a7d99977ed8c
Flynn Graham <flynngraham101@gmail.com>
2023-02-28 16:45:39 +0000

    Base Interface Cleaned

--- Main.py
+++ Main.py
@@ -10,6 +10,32 @@ class Main:
         print("\nLegal Move List : " + str(self.board.legal_moves)[37:-1])
         pass
 
+    def resetGame(self):
+        #reset the board
+        self.board.reset
+        #start another game
+        self.startGame()
+    
+    def gameOver(self):
+        print(self.board)
+        print(self.board.outcome)
+
+    def playMove(self, colour, turnFirst):
+        if turnFirst:
+            if colour == "w":
+                self.playHumanMove()
+            
+            elif colour == "b":
+                self.playEngineMove(ch.BLACK)
+            
+        elif not turnFirst:
+            if colour == "w":
+                self.playEngineMove(ch.WHITE)
+            
+            elif colour == "b":
+                self.playHumanMove()
+
+
     #Player's Turn
     def playHumanMove(self):
         try:
@@ -22,43 +48,51 @@ class Main:
 
         except:
             self.playHumanMove()
+            
+            print(self.board)
+            print(self.board.outcome())  
+
 
     #Engine's Turn
-    def playEngineMove(self, maxDepth, color):
-        engine = ce.Engine(self.board, maxDepth, color)
+    def playEngineMove(self, colour):
+        print("The engine is thinking...")
+
+        engine = ce.Engine(self.board, self.maxDepth, colour)
 
         self.board.push(engine.getBestMove())
 
     #start a game
     def startGame(self):
-        #get human player's color
-        color, maxDepth = None, None
-        while color != "b" and color != "w" and not isinstance(maxDepth, int):
-            color = input("Play as (type 'b' or 'w'): ")
-            maxDepth = int(input("Choose depth: "))+1
+        # Get player information
 
-        if color=="b":
-            while (self.board.is_checkmate()==False):
-                print("The engine is thinking...")
-                self.playEngineMove(maxDepth, ch.WHITE)
+        colour, self.maxDepth = None, None
+        while colour != "b" and colour != "w" and not isinstance(self.maxDepth, int):
+            colour = input("Play as (type 'b' or 'w'): ")
+            self.maxDepth = int(input("Choose depth: "))+1 # Note: Depth needs to be bumped by 1 to prevent errors
+
+        print(self.board)
+
+
+        while not self.board.is_checkmate():
+
+            if colour == "b":
+                self.playEngineMove(ch.WHITE)
                 print(self.board)
+
                 self.playHumanMove()
                 print(self.board)
-            print(self.board)
-            print(self.board.outcome())    
-        elif color=="w":
-            while (self.board.is_checkmate()==False):
-                print(self.board)
+
+            if colour == "w":
                 self.playHumanMove()
                 print(self.board)
-                print("The engine is thinking...")
-                self.playEngineMove(maxDepth, ch.BLACK)
-            print(self.board)
-            print(self.board.outcome())
-        #reset the board
-        self.board.reset
-        #start another game
-        self.startGame()
+
+                self.playEngineMove(ch.BLACK)
+                print(self.board)
+            
+
+
+        self.gameOver()
+        self.resetGame()
 
 #create an instance and start a game
 newBoard= ch.Board()
